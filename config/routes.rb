a44bdb0bc8c55f1ab7f4ece4497be8d6b9599a58
Princepal3467 <psingh60@academic.rrc.ca>
2026-03-26 13:21:06 -0500

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
+  get "categories/show"
   root "products#index"
 
   get "about", to: "pages#about"
 
-  resources :products
-  resources :orders
-
+  resources :products, only: [:index, :show]
+  resources :orders, only: [:index]
   post "add_to_cart/:id", to: "orders#add_to_cart", as: "add_to_cart"
+
+  get "categories/:id", to: "categories#show", as: "category"
+
+  namespace :admin do
+    get "/", to: "dashboard#index", as: "dashboard"
+    resources :products
+    resources :categories
+  end
 end
