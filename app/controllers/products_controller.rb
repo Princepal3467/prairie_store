0859ed68408060a9f2cdfcd3b4dd3cf7bf8a5148
Princepal3467 <psingh60@academic.rrc.ca>
2026-03-26 13:21:37 -0500

    Add files via upload

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,15 @@
 class ProductsController < ApplicationController
   def index
+    @categories = Category.all
     @products = Product.all
+
+    if params[:search].present?
+      @products = @products.where("name LIKE ? OR description LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%")
+    end
+
+    if params[:category_id].present?
+      @products = @products.where(category_id: params[:category_id])
+    end
   end
 
   def show
